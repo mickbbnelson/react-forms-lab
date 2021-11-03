54980b4c96a3fa1613e95e7a4e8e208355ab3a8d
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-11-03 11:04:57 -0400

    Question for 1 on 1

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -24,9 +24,9 @@ class LoginForm extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    if (!this.state.username && !this.state.password) {
+    //Figure out the correct way to implement Boolean for this
     this.props.handleLogin(this.state) 
-  }}
+  }
 
   render() {
     return (
